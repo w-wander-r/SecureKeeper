f0047f7ed4194d25af4b048db5b08d5e77c33475
w-wander-r <wanderexe68@gmail.com>
2025-01-18 11:48:24 +0200

    connected db

---
+++ backend/SecureKeeper/src/main/java/SecureKeeper/SecureKeeperApplication.java
@@ -0,0 +1,13 @@
+package SecureKeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureKeeperApplication.class, args);
+	}
+
+}
