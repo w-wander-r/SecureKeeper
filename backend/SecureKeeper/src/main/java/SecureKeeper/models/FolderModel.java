1a0c004bf2193229ac56fa77939ae41f6217bb9a
w-wander-r <wanderexe68@gmail.com>
2025-02-07 23:27:53 +0200

    two entities added for future implementation.(might be changed)

---
+++ backend/SecureKeeper/src/main/java/SecureKeeper/models/FolderModel.java
@@ -0,0 +1,50 @@
+package SecureKeeper.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "folders")
+public class FolderModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private UsersModel user;
+    private String name;
+
+    public FolderModel() {
+    }
+    public FolderModel(Long id, UsersModel user, String name) {
+        this.id = id;
+        this.user = user;
+        this.name = name;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public UsersModel getUser() {
+        return user;
+    }
+    public void setUser(UsersModel user) {
+        this.user = user;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ backend/SecureKeeper/src/main/java/SecureKeeper/models/NoteModel.java
@@ -0,0 +1,56 @@
+package SecureKeeper.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "notes")
+public class NoteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "folder_id", nullable = false)
+    private FolderModel folder;
+
+    private String title;
+    private String email;
+    private String username;
+    private String password;
+    
+    public NoteModel() {
+    }
+    public NoteModel(Long id, FolderModel folder, String title, String email, String username, String password) {
+        this.id = id;
+        this.folder = folder;
+        this.title = title;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+    public Long getId() {
+        return id;
+    }
+    public FolderModel getFolder() {
+        return folder;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+}
