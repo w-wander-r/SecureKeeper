1b6b2420cd45aadbb9f92cdd3f387bae2670f966
w-wander-r <wanderexe68@gmail.com>
2025-02-14 01:24:39 +0200

    changed approach for users entity

--- backend/SecureKeeper/src/main/java/SecureKeeper/models/UsersModel.java
+++ backend/SecureKeeper/src/main/java/SecureKeeper/models/UsersModel.java
@@ -1,10 +1,17 @@
 package SecureKeeper.models;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -18,15 +25,35 @@ public class UsersModel {
 
     private String username;
     private String password;
-    
+
+    /* 
+
+     * First approach: not using @OneToMany to keep simplicity
+     *  and could be better performance as folders not loading
+     * but there some drawbacks ->
+     *  -> Indirect Access, which can cause lower performance
+     *  -> Lack Of Cascading - TODO: test this case(deleting user)
+     
+    */
+
+    /* 
+      
+     * Second approach: use @OneToMany
+     *  Most likely its best approach for now
+      
+    */
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Folder> folders;
 
     // Constructors
     public UsersModel() {}
 
-    public UsersModel(Long id, String username, String password) {
+    public UsersModel(Long id, String username, String password, List<Folder> folders) {
         this.id = id;
         this.username = username;
         this.password = password;
+        this.folders = folders;
     }
 
     // Getters and Setters
@@ -38,6 +65,9 @@ public class UsersModel {
     
     public String getPassword() {return password;}
     public void setPassword(String password) {this.password = password;}
+
+    public List<Folder> getFolders() {return folders;}
+    public void setFolders(List<Folder> folders) {this.folders = folders;}
 
     @Override
     public String toString() {
