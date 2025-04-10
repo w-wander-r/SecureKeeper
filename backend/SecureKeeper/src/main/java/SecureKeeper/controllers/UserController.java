e73be476f7f3c76c3b41f99afd17256e7b680622
w-wander-r <wanderexe68@gmail.com>
2025-04-10 14:18:24 +0300

    cross origin added for frontend

--- backend/SecureKeeper/src/main/java/SecureKeeper/controllers/UserController.java
+++ backend/SecureKeeper/src/main/java/SecureKeeper/controllers/UserController.java
@@ -1,6 +1,7 @@
 package SecureKeeper.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import SecureKeeper.service.UserService;
 
 // Endpoints for Login and Register
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class UserController {
 
     @Autowired
