de7f6f21950a5571ac1e840f8d748f67e39ea7a1
happy-1488 <viter.rostyslav1124@vu.cdu.edu.ua>
2025-05-02 18:15:00 +0300

    refactor: enhance login form validation and error handling

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import './_login.scss';
 import { LogoIcon, LoginIcon } from '../icons/Icons';
@@ -11,6 +11,41 @@ const Login = () => {
   const [passwordDirty, setPasswordDirty] = useState(false);
   const [usernameError, setUsernameError] = useState('Username cannot be empty');
   const [passwordError, setPasswordError] = useState('Password cannot be empty');
+  const [counter, setCounter] = useState(0);
+  const [formValid, setFormValid] = useState(false);
+
+  useEffect(() => {
+    if (usernameError !== '' || passwordError !== '') {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [usernameError, passwordError]);
+
+  const usernameHandler = (e) => {
+    const value = e.target.value;
+    setUsername(value);
+    const re = /^[a-zA-Z0-9]+([._]?[a-zA-Z0-9]+)*$/;
+    if (value.length === 0) {
+      setUsernameError('Username cannot be empty');
+    } else if (!re.test(value)) {
+      setUsernameError('Incorrect username');
+    } else {
+      setUsernameError('');
+    }
+  };
+
+  const passwordHandler = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+    if (value.length === 0) {
+      setPasswordError('Password cannot be empty');
+    } else if (value.length < 8 || value.length > 20) {
+      setPasswordError('Password must be between 8 and 20 characters');
+    } else {
+      setPasswordError('');
+    }
+  }
 
   const blurHandler = (e) => {
     switch (e.target.name) {
@@ -33,17 +68,21 @@ const Login = () => {
 
       <div className='signup-form'>
         <form className='signup-form__form'>
-          <label className="signup-form__label">Username
-          {(usernameDirty && usernameError) && <span>{usernameError}</span>}
-          </label>
-          <input name="username" onBlur={e => blurHandler(e)} type='text' className="signup-form__input" maxLength={20} required />
+          <label className="signup-form__label">Username</label>
+          <input onChange={usernameHandler} value={username} name="username" onBlur={e => blurHandler(e)} type='text' className="signup-form__input" required />
+          <div className="signup-form__error">
+            {usernameDirty && usernameError && <span>{usernameError}</span>}
+          </div>
       
           <label htmlFor="password-login" className="signup-form__label">Password
-          {(passwordDirty && passwordError) && <span>{passwordError}</span>}
+            <span className='signup-form__input-counter' style={{ color: counter < 8 || counter > 20 ? "red" : "green" }}>{counter}/20</span>
           </label>
-          <input name="password" onBlur={e => blurHandler(e)} type='password' className="signup-form__input" maxLength={20} required />
+          <input onChange={passwordHandler} value={password} name="password" onBlur={e => blurHandler(e)} onInput={e => setCounter(e.target.value.length)} type='password' className="signup-form__input" minLength={8} maxLength={20} required />
+          <div className="signup-form__error">
+            {passwordDirty && passwordError && <span>{passwordError}</span>}
+          </div>
 
-          <button className="signup-form__btn">Sign up</button>
+          <button disabled={!formValid} type='submit' className="signup-form__btn" style={{ cursor: !formValid ? 'not-allowed' : 'pointer' }}>Sign up</button>
         </form>
 
         <span className='or'>Or</span>
