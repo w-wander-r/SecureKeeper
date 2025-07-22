57566d669dd728923b1754828796a0a59ae5b781
w-wander-r <wanderexe68@gmail.com>
2025-07-22 11:53:06 +0300

    Changed key for encryption and moved to env file

--- backend/SecureKeeper/src/main/java/SecureKeeper/service/EncryptionService.java
+++ backend/SecureKeeper/src/main/java/SecureKeeper/service/EncryptionService.java
@@ -1,37 +1,45 @@
 package SecureKeeper.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.SecureRandom;
 import java.util.Base64;
 
 @Service
 public class EncryptionService {
+    // TODO fix .algorithm
+    private final String algorithm = "AES/CBC/PKCS5Padding";
+    private final SecretKeySpec secretKey;
+    private final IvParameterSpec iv;
 
-    // TODO change algo
-    private static final String AES = "AES";
-    private final SecretKey secretKey;
+    public EncryptionService(
+//            @Value("${encryption.algorithm") String algorithm,
+            @Value("${encryption.key}") String base64Key,
+            @Value("${encryption.iv}") String base64Iv
+    ) {
+//        this.algorithm = algorithm.trim();
+        byte[] keyBytes = Base64.getDecoder().decode(base64Key);
+        byte[] ivBytes = Base64.getDecoder().decode(base64Iv);
 
-    public EncryptionService() throws Exception {
-        String secretString = "my32ByteKey123456789012345678901"; // key for testing. TODO
-        byte[] keyBytes = secretString.getBytes();
-        this.secretKey = new SecretKeySpec(keyBytes, AES);
+        this.secretKey = new SecretKeySpec(keyBytes, "AES");
+        this.iv = new IvParameterSpec(ivBytes);
     }
 
     public String encrypt(String data) throws Exception {
-        Cipher cipher = Cipher.getInstance(AES);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        byte[] encryptedBytes = cipher.doFinal(data.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedBytes);
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, iv);
+        byte[] encrypted = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
     }
 
     public String decrypt(String encryptedData) throws Exception {
-        Cipher cipher = Cipher.getInstance(AES);
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        byte[] decodedBytes = Base64.getDecoder().decode(encryptedData);
-        byte[] decryptedBytes = cipher.doFinal(decodedBytes);
-        return new String(decryptedBytes);
+        Cipher cipher = Cipher.getInstance(algorithm);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, iv);
+        byte[] decoded = Base64.getDecoder().decode(encryptedData);
+        byte[] decrypted = cipher.doFinal(decoded);
+        return new String(decrypted);
     }
 }
